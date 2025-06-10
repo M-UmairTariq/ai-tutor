8b6329f276e98200f184030fc776bc327a34a461
Salman Khan <sk5926255@gmail.com>
2025-06-10 19:00:41 +0500

    changed websocket to socket-io

--- src/config/ApiConfig.tsx
+++ src/config/ApiConfig.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL + "/api/v1";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClient = axios.create({
     baseURL,
