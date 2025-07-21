e972c93dd0637c67e64082a6c6d1b644612480da
Salman Khan <sk5926255@gmail.com>
2025-07-21 23:54:29 +0500

    env

--- src/pages/student/ChatWindow.tsx
+++ src/pages/student/ChatWindow.tsx
@@ -318,7 +318,7 @@ const ChatWindow: React.FC<ChatWindowProps> = ({
   const mode = searchParams.get("mode");
   const userData = JSON.parse(localStorage.getItem("AiTutorUser") || "{}");
   const userId = userData?.id;
-  const SOCKET_URL = "https://tutorapp-cyfeg4ghe7gydbcy.uaenorth-01.azurewebsites.net";
+  const SOCKET_URL = "https://malamute-content-cougar.ngrok-free.app";
   const resetActivityTimer = useCallback(() => {
     if (activityTimerRef.current) clearTimeout(activityTimerRef.current);
     activityTimerRef.current = setTimeout(() => {
